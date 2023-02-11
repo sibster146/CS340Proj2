cc5e7994ba8d48256fe2b0f5349fbc32b11e0655
Siby Suriyan <sibysuriyancareer@gmail.com>
2023-02-11 02:29:03 -0600

    just need to add the fin ack stuff for part 3

--- streamer.py
+++ streamer.py
@@ -5,6 +5,7 @@ from socket import INADDR_ANY
 import struct
 import heapq
 import concurrent.futures
+import threading
 import time
 
 """
@@ -15,8 +16,10 @@ import time
 class Streamer:
     def __init__(self, dst_ip, dst_port,
                  src_ip=INADDR_ANY, src_port=0):
-        """Default values listen on all network interfaces, chooses a random source port,
-           and does not introduce any simulated packet loss."""
+        """
+        Default values listen on all network interfaces, chooses a random source port,
+        and does not introduce any simulated packet loss.
+        """
         self.socket = LossyUDP()
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
@@ -25,9 +28,19 @@ class Streamer:
         self.ackNum = 0
         self.seqNum = 0
         self.closed = False
+
         self.ack = False
-        executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-        executor.submit(self.listener)
+        self.future = None
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        self.start_listener()
+
+    def start_listener(self):
+        self.future = self.executor.submit(self.listener)
+
+    def stop_listener(self):
+        if self.future is not None:
+            self.future.cancel()
+            self.executor.shutdown()
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
@@ -36,26 +49,28 @@ class Streamer:
         lenBytes = len(data_bytes)
         
         for i in range(0,lenBytes,1468):
+
             self.ack = False
             packetHeader = struct.pack("!i",self.seqNum)
             self.socket.sendto(packetHeader + data_bytes[i:i+1468], (self.dst_ip, self.dst_port))
             start = time.perf_counter()
             while not self.ack:
-                if time.perf_counter()-start>=0.25 and not self.ack:
-                    self.ack = False
+                if time.perf_counter()-start>=.5 and not self.ack:
+                    #self.ack = False
                     self.socket.sendto(packetHeader + data_bytes[i:i+1468], (self.dst_ip, self.dst_port))
                     start = time.perf_counter()
-                print(time.perf_counter()-start)
+                    #time.sleep(0.01)
+            
 
-                #time.sleep(0.01)
             self.seqNum+=1
+
+
 
 
     """
     THERE ARE TWO WAYS TO SEND ACKs
     1. SEND THE ACKS in the RECV FUNCTION- tried this it didnt work too well
     2. SEND THE ACKS IN THE LISTENER FUNCTION- trying this one now
-
     """
 
 
@@ -69,12 +84,13 @@ class Streamer:
         while 1:
             if self.ackNum in self.buffer:
                 val = self.buffer[self.ackNum]
+                self.buffer[self.ackNum]
                 del self.buffer[self.ackNum]
-                #self.socket.sendto(ackHeader + val, (self.dst_ip, self.dst_port) )
-
                 self.ackNum+=1
+                #self.socket.sendto(ackHeader + val, (self.dst_ip, self.dst_port) )
                 return val
-                """
+                        
+            """
             else:
                 packet,addr = self.socket.recvfrom()
                 num = struct.unpack('!i',packet[:4])[0]
@@ -82,31 +98,69 @@ class Streamer:
                 #print(f"This is acknum: {self.ackNum}")
                 #print(f"This is the packet: {num},{data},{type(data)}")
                 self.buffer[num] = data
-                """
-                
+            """
+
+
     def listener(self):
         ackHeader = struct.pack("!i",-1)
+        ack2Header = struct.pack("!i",-2)
+        lastSeqNum = []
         while not self.closed: # a later hint will explain self.closed
             try:
                 packet, addr = self.socket.recvfrom()
                 num = struct.unpack('!i',packet[:4])[0]
                 data = packet[4:]
-                
-                if num==-1:
 
+                if num == -1:
                     self.ack = True
+
+
+
+
+                elif num >= 0:
+
+                    if (lastSeqNum and lastSeqNum[-1]!=num-1) and (num not in lastSeqNum):
+                        continue
+
+                    lastSeqNum.append(num)
+                    self.buffer[num] = data
+                    self.socket.sendto(ackHeader + data, (self.dst_ip, self.dst_port))
+
+
+                  
+
+      # store the data in the receive buffer
+            except Exception as e:
+                print("listener died!")
+                print(e)
+
+
+    """"         
+    def listener(self):
+        ackHeader = struct.pack("!i",-1)
+
+        while not self.closed:
+            try:
+                packet, addr = self.socket.recvfrom()
+                num = struct.unpack("!i",packet[:4])
+                if num==-1:
+                    check = struct.unpack('!i',packet[4:])
+                    if check == self.ackNum:
+                        self.ack = True
+                        self.ackNum+=1
                 else:
                     data = packet[4:]
-                    print(data)
-                    self.socket.sendto(ackHeader + data, (self.dst_ip, self.dst_port))
                     self.buffer[num] = data
+                    ackNumCheck = struct.pack("!i", self.ackNum)
+                    self.socket.sendto(ackHeader + ackNumCheck, (self.dst_ip, self.dst_port))
+
 
       # store the data in the receive buffer
       # ...
             except Exception as e:
                 print("listener died!")
                 print(e)
-
+ """
 
 
     def close(self) -> None:
